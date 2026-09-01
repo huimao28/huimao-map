f69031a5dabf22b86d11c79ba2406f4d624d6a34
huimao28 <huimao28@users.noreply.github.com>
2026-09-01 11:45:27 +0800

    修复Wear OS通知权限改动导致的编译错误

--- wear/src/main/java/com/huimao/map/wear/MainActivity.kt
+++ wear/src/main/java/com/huimao/map/wear/MainActivity.kt
@@ -5,6 +5,7 @@ import android.Manifest
 import android.content.pm.PackageManager
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
